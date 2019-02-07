6585344b158d1ab59427b35ecbd16a08b7a75993
unesgit <unes82@gmail.com>
2019-02-07 10:45:24 +0000

    adding log

--- authBasic/authBasic/src/main/java/com/example/authBasic/controller/LoginController.java
+++ authBasic/authBasic/src/main/java/com/example/authBasic/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.example.authBasic.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +13,15 @@ import com.example.authBasic.model.Login;
 @Controller
 public class LoginController {
     
+    Logger logger = LoggerFactory.getLogger(LoginController.class);
+    
     @RequestMapping(value = { "/" }, method = RequestMethod.GET)
     public ModelAndView projectBase() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
         return new ModelAndView("redirect:/dashboard");
     }
     
